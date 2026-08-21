bed03fd20f5bf6c23e72c46945adc78b0ba7b229
schnellsoft <deltasoft7@gmail.com>
2026-08-21 09:22:31 +0300

    aaab

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Title from './features/header/components/title/Title'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Title title="Hi Dan, welcome to the app" />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
